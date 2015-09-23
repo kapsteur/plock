a7f6254f0d6e45ac30885485cf15be3849c8caf2
Garry POUPIN <kapsteur@gmail.com>
2015-09-23 23:06:55 +0200

    now manage block by tabs

--- plock.js
+++ plock.js
@@ -1,65 +1,138 @@
 hosts = [
-  '.a2dfp',
-  '.ad6media',
-  '.adbrite',
-  '.adcash',
-  '.adknowledge',
-  '.adnext',
-  '.adnxs',
-  '.adsdk',
-  '.atdmt',
-  '.atwola',
-  '.bfast',
-  '.beead',
-  '.criteo',
-  '.ezakus',
-  '.falkag',
-  '.himediads',
-  '.infolinks',
-  '.kameleoon',
-  '.ligatus',
-  '.outbrain',
-  '.qksrv',
-  '.skimresources',
-  '.tradedoubler',
-  '.tradelab',
-  '.visualrevenue',
-  '.doubleclick',
-  '.blogads',
-  '.click-fr',
-  '.ebz.io',
-  '.edi7',
-  '.eficiens-',
-  '.media-clic',
-  '.netavenir',
-  '.piximedia',
-  '.promobenef',
-  '.pubdirecte',
-  '.rentabiliweb',
-  '.sascdn.com',
-  '.adverpub',
-  '.deltapub',
-  '.gestionpub',
-  '.manchettepub',
-  '.regiedepub',
-  '.regypub',
-  '.storpub',
-  '.googleadservices',
-  '.googlesyndication',
-  '.clickthru',
-  '.taboola',
+  'googleadservices',
+  'googlesyndication',
+  'a2dfp',
+  'ad6media',
+  'adbrite',
+  'adcash',
+  'adknowledge',
+  'adnext',
+  'adnxs',
+  'adsdk',
+  'atdmt',
+  'atwola',
+  'bfast',
+  'beead',
+  'criteo',
+  'ezakus',
+  'falkag',
+  'himediads',
+  'infolinks',
+  'kameleoon',
+  'ligatus',
+  'outbrain',
+  'qksrv',
+  'skimresources',
+  'tradedoubler',
+  'tradelab',
+  'visualrevenue',
+  'doubleclick',
+  'blogads',
+  'click-fr',
+  'ebz.io',
+  'edi7',
+  'eficiens-',
+  'media-clic',
+  'netavenir',
+  'piximedia',
+  'promobenef',
+  'pubdirecte',
+  'rentabiliweb',
+  'sascdn.com',
+  'adverpub',
+  'deltapub',
+  'gestionpub',
+  'manchettepub',
+  'regiedepub',
+  'regypub',
+  'storpub',
+  'clickthru',
+  'taboola',
+  'smartadserver.com',
+  'r66net.com',
+  'videostep.com',
+  'scorecardresearch.com',
 ];
 
-var rules = {
-  conditions: new Array(hosts.length),
-  actions: [
-    new chrome.declarativeWebRequest.CancelRequest()
-  ]};
+var tabs = {};
 
-for (var i=0; i<hosts.length; i++) {
-  rules.conditions[i] = new chrome.declarativeWebRequest.RequestMatcher({
-    url: { hostContains: hosts[i] } }
-  )
+//Init tabs
+chrome.tabs.query({}, function(allTabs) {
+  for (var i=0; i<allTabs.length; i++) {
+    var tab = allTabs[i];
+    if (tabs[tab.id] == undefined) {
+      tabs[tab.id] = {'block':true, 'url':tab.url, 'count':0};
+    };
+  }
+});
+
+//Update tabs
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
+    if (tabs[tab.id] == undefined) {
+      tabs[tab.id] = {'block':true, 'url':tab.url, 'count':0};
+    };
+    if (tabs[tab.id]['block']) {
+      chrome.browserAction.setIcon({path: "plock.png"});
+    } else {
+      chrome.browserAction.setIcon({path: "plock_ok.png"});
+    }
+    //updateCounter();
+  });
+});
+
+//Update tabs url
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
+    if (changeInfo.url != undefined) {
+      tabs[tabId]['url'] = changeInfo.url;
+      tabs[tabId]['count'] = 0;
+      updateCounter();
+    }
+});
+
+//Update tabs block
+chrome.browserAction.onClicked.addListener(function(tab) {
+  if (tabs[tab.id]['block']) {
+    tabs[tab.id]['block'] = false;
+    chrome.browserAction.setIcon({path: "plock_ok.png"});
+  } else {
+    tabs[tab.id]['block'] = true;
+    chrome.browserAction.setIcon({path: "plock.png"});
+  }
+  chrome.tabs.reload(tab.id);
+});
+
+var block = function(details) {
+  //console.log(details);
+  if (tabs[details.tabId] != undefined && tabs[details.tabId]['block']) {
+    var parser = document.createElement('a');
+    parser.href = details.url;
+
+    //console.log("hostname:"+parser.hostname);
+    for (var i=0; i<hosts.length; i++) {
+      if (parser.hostname.indexOf(hosts[i]) != -1) {
+        tabs[details.tabId]['count']++;
+        updateCounter();
+        return {cancel:true};
+      }
+    }
+    return {};
+  }
 }
 
-chrome.declarativeWebRequest.onRequest.addRules([rules]);
+chrome.webRequest.onBeforeRequest.addListener(
+  block,
+  {urls: ["<all_urls>"]},
+  ["blocking"]);
+
+function updateCounter() {
+  chrome.tabs.query({'active': true}, function(allTabs) {
+    var tab = allTabs[0];
+    if (tabs[tab.id]['count']>0) {
+      chrome.browserAction.setBadgeBackgroundColor({color:[255, 0, 0, 255], tabId:tab.id});
+      chrome.browserAction.setBadgeText({text: ""+tabs[tab.id]['count'], tabId:tab.id});
+    } else {
+      chrome.browserAction.setBadgeText({text: "", tabId:tab.id});
+    }
+  });
+}
